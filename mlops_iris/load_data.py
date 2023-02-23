6aad0e62bf8d3c33428d4348ce46c4324f23d57c
Rajat <rajatrushiya2013@outlook.com>
2023-02-23 19:44:27 +0530

    spark adde

--- mlops_iris/load_data.py
+++ mlops_iris/load_data.py
@@ -5,11 +5,12 @@ from sklearn.model_selection import train_test_split
 def load_split_data(file_path):
     columns = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width', 'Class_labels'] # As per the iris dataset information
 
-    df_test=pd.read_csv(file_path)
-    print(df_test)
+    # df_test=pd.read_csv(file_path)
+    # print(df_test)
                         
     # Load the data
-    df = pd.read_csv(file_path, names=columns)
+    # df = pd.read_csv(file_path, names=columns)
+    df = file_path
 
     df.head()
 

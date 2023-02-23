b324a10f81771f02a7b31a5961008bce1941dcb4
Rajat <rajatrushiya2013@outlook.com>
2023-02-23 19:12:32 +0530

    spark adde

--- notebooks/predrive.py
+++ notebooks/predrive.py
@@ -6,13 +6,13 @@ from mlops_iris import predict
 
 
 # Read the CSV file into a Spark DataFrame
-df_spark = spark.read.csv("/dbfs/FileStore/data/01_raw/iris.data", header=True, inferSchema=True)
+df_spark = spark.read.csv("dbfs:/FileStore/data/01_raw/iris.data", header=True, inferSchema=True)
 
 # Write the Spark DataFrame to Delta format
-df_spark.write.format("delta").mode("overwrite").save("/dbfs/FileStore/data/02_raw/")
+df_spark.write.format("delta").mode("overwrite").save("dbfs:/FileStore/data/delta/")
 
 # Read the Delta table into a Spark DataFrame
-df_spark = spark.read.format("delta").load("/dbfs/FileStore/data/02_raw/")
+df_spark = spark.read.format("delta").load("dbfs:/FileStore/data/delta/")
 
 # Convert the Spark DataFrame to a Pandas DataFrame and pass it to the load_split_data function
 train_df_pandas, test_df_pandas = load_data.load_split_data(df_spark.toPandas())

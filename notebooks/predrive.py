0cbc698be0aebcc494e984e73f1a60818e048491
Rajat <rajatrushiya2013@outlook.com>
2023-02-23 19:07:46 +0530

    spark adde

--- notebooks/predrive.py
+++ notebooks/predrive.py
@@ -4,10 +4,6 @@ from mlops_iris import load_data
 from mlops_iris import train
 from mlops_iris import predict
 
-from pyspark import *
-
-# Create a SparkSession
-spark = SparkSession.builder.appName("LoadAndSplitData").getOrCreate()
 
 # Read the CSV file into a Spark DataFrame
 df_spark = spark.read.csv("/dbfs/FileStore/data/01_raw/iris.data", header=True, inferSchema=True)
@@ -21,5 +17,4 @@ df_spark = spark.read.format("delta").load("/dbfs/FileStore/data/02_raw/")
 # Convert the Spark DataFrame to a Pandas DataFrame and pass it to the load_split_data function
 train_df_pandas, test_df_pandas = load_data.load_split_data(df_spark.toPandas())
 
-# Stop the SparkSession
-spark.stop()
+

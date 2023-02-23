b19c9a323fa9f3980721f9c6f9428bbcaec5ca03
Rajat <rajatrushiya2013@outlook.com>
2023-02-23 18:44:02 +0530

    spark adde

---
+++ notebooks/predrive.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+
+from mlops_iris import load_data
+from mlops_iris import train
+from mlops_iris import predict
+
+# Create a SparkSession
+spark = SparkSession.builder.appName("LoadAndSplitData").getOrCreate()
+
+# Read the CSV file into a Spark DataFrame
+df_spark = spark.read.csv("/dbfs/FileStore/data/01_raw/iris.data", header=True, inferSchema=True)
+
+# Write the Spark DataFrame to Delta format
+df_spark.write.format("delta").mode("overwrite").save("/dbfs/FileStore/data/02_raw/")
+
+# Read the Delta table into a Spark DataFrame
+df_spark = spark.read.format("delta").load("/dbfs/FileStore/data/02_raw/")
+
+# Convert the Spark DataFrame to a Pandas DataFrame and pass it to the load_split_data function
+train_df_pandas, test_df_pandas = load_data.load_split_data(df_spark.toPandas())
+
+# Stop the SparkSession
+spark.stop()
